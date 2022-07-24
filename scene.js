b251dec7aea2b284af233bc964cdd41b6915e827
Stepan Legachev <gitlog@sib.li>
2022-07-25 01:17:50 +0200

    feat: add three.js basic example
    https://threejs.org/docs/#manual/en/introduction/Creating-a-scene

---
+++ scene.js
@@ -0,0 +1,30 @@
+import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
+
+import {svg} from './data.js';
+
+
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new BoxGeometry( 1, 1, 1 );
+const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
+animate();
+
+console.log('DONE');
