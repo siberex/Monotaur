7bd3ee50fae7bdf9f57da132db476099a1b1851b
Stepan Legachev <gitlog@sib.li>
2022-07-24 23:19:34 +0200

    docs: update readme

---
+++ data.js
@@ -0,0 +1,12 @@
+const svg = [
+    '<svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" viewBox="0 0 660 1100"><path d="M660 0H0v1100h660zM440 220H220v660h220z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 1100"><path d="M220 220v660H0v220h660V880H440V0H0v220z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 1100"><path d="M220 660h440V0H0v220h440v220H0v660h660V880H220z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 1100"><path d="M0 1100h660V0H0v220h440v220H220v220h220v220H0z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 1100"><path d="M440 440H220V0H0v660h440v440h220V0H440z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 1100"><path d="M220 440h440v660H0V880h440V660H0V0h660v220H220z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" viewBox="0 0 660 1100"><path d="M0 1100V0h660v220H220v220h440v660zm220-220V660h220v220z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 1100"><path d="M660 1100V0H0v220h440v880z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" viewBox="0 0 660 1100"><path d="M660 0v1100H0V0zM220 880V660h220v220zm220-660v220H220V220z"/></svg>',
+    '<svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" viewBox="0 0 660 1100"><path d="M660 0v1100H0V880h440V660H0V0zM440 220v220H220V220z"/></svg>',
+];
