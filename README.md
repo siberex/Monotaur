aea6a7f8dfe85253f756c73a7c4ac516975ac3a9
Stepan Legachev <gitlog@sib.li>
2022-07-25 02:59:47 +0200

    feat: add surface extrusion from SVG shape (aka making I-beam from `I`)

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Monotaur
 
-Exploration project to check Three.js features.
+Exploration project to check Three.js features and capabilities.
 
 Goals:
 
-- Extrude 3D surfaces from SVG shapes.
+- [x] Extrude 3D surfaces from SVG shapes.
 
     Example: Extrude I-beam surface from `I` shape.
 
-  - Tricky part: cut-out shapes with subtracted geometry.
+  - [ ] Tricky part: cut-out shapes with subtracted geometry.
 
     Example: Extrude ring surface from `O` shape.
 
-- Boolean Intersect two perpendicular surfaces extracted that way.
+- [ ] Boolean Intersect two perpendicular surfaces extracted that way.
+
+- [x] Simple rotational animation.
+
+- [ ] Advanced animation with changing shape in the middle.
--- scene.js
+++ scene.js
@@ -1,7 +1,21 @@
-import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
-import WebGL from './node_modules/three/examples/jsm/capabilities/WebGL.js';
+import {
+    AxesHelper,
+    Box3,
+    ExtrudeGeometry,
+    Group,
+    Mesh,
+    MeshNormalMaterial,
+    PerspectiveCamera,
+    Scene,
+    Vector3,
+    WebGLRenderer
+} from 'three';
+import {SVGLoader} from 'three/examples/jsm/loaders/SVGLoader.js';
+import WebGL from 'three/examples/jsm/capabilities/WebGL.js';
 
 import {svg} from './data.js';
+
+const DEBUG = true;
 
 // Init three.js scene
 const scene = new Scene();
@@ -19,19 +33,88 @@ window.addEventListener('resize', function(e) {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// Render shapes
-const geometry = new BoxGeometry( 1, 1, 1 );
-const material = new MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-const cube = new Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+// Load SVG and extrude surface from SVG paths
+// https://threejs.org/docs/#examples/en/loaders/SVGLoader
+const loader = new SVGLoader();
+const svgData = loader.parse(svg[1]);
+
+// Group we'll use for all SVG paths
+const group = new Group();
+// When importing SVGs paths are inverted on Y axis
+// it happens in the process of mapping from 2d to 3d coordinate system
+group.scale.y *= -1;
+
+const scaleFactor = 0.5;
+group.scale.multiplyScalar( scaleFactor );
+
+const material = new MeshNormalMaterial({ wireframe: DEBUG });
+
+// Loop through all the parsed paths
+svgData.paths.forEach((path, i) => {
+    const shapes = path.toShapes(true);
+
+    // Each path has array of shapes
+    shapes.forEach((shape, j) => {
+        if (j > 0) {
+            // More than one path probably means subtracted geometry (cutaway shape)
+            // ...
+        }
+
+        // Finally we can take each shape and extrude it
+        const geometry = new ExtrudeGeometry(shape, {
+            steps: 1,
+            depth: 660,
+            bevelEnabled: false
+        });
+
+        // Create a mesh and add it to the group
+        const mesh = new Mesh(geometry, material);
+
+        group.add(mesh);
+    });
+});
+
+
+// Meshes we got are all relative to themselves
+// meaning they have position set to (0, 0, 0)
+// which makes centering them in the group easy
+
+// Get group's size
+const box = new Box3().setFromObject(group);
+let vectorSize = new Vector3();
+box.getSize(vectorSize);
+
+// Revere group scaleFactor for correct children offsetting
+vectorSize.multiplyScalar(1 / scaleFactor);
+
+// Offset each dimension half its length to center group elements
+vectorSize.multiplyScalar(-0.5);
+group.children.forEach(item => {
+    item.position.copy(vectorSize);
+});
+
+// Axes helper
+if (DEBUG) {
+    const axesHelper = new AxesHelper(500);
+    group.add(axesHelper);
+}
+
+// Finally we add svg group to the scene
+scene.add(group);
+
+
+// const geometry = new BoxGeometry( 1, 1, 1 );
+// const material = new MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+// const cube = new Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 2000;
 
 function animate() {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    group.rotation.y += 0.01;
 
     renderer.render( scene, camera );
 }
