e6251ca6e80e5aad7d67194eecbdabfd16494a1d
Stepan Legachev <gitlog@sib.li>
2022-07-27 01:52:03 +0200

    Animation for switching models (#3)
    * add binary intersection for pairs of extruded meshes
    
    * refactor: add centerOrigin flag for MeshFromPath()
    
    * add OrthographicCamera for smooth transitions
    
    * feat: calc rotation quadrant
    
    * feat: add digit transition animation by switching mesh upon leaving rotation quadrant
    
    * refactor: simplify and cleanup

--- scene.js
+++ scene.js
@@ -1,11 +1,10 @@
 import {
-    AxesHelper,
-    Box3,
     ExtrudeGeometry,
     Group,
     MathUtils,
     Mesh,
     MeshNormalMaterial,
+    OrthographicCamera,
     PerspectiveCamera,
     Scene,
     Vector3,
@@ -15,30 +14,89 @@ import {SVGLoader} from 'three/examples/jsm/loaders/SVGLoader.js';
 import WebGL from 'three/examples/jsm/capabilities/WebGL.js';
 import { CSG } from 'three-csg-ts';
 
-import {svg} from './data.js';
+
+let SCREEN_WIDTH = window.innerWidth;
+let SCREEN_HEIGHT = window.innerHeight;
+let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+const frustumSize = 1400;
 
 // Init three.js scene
 const scene = new Scene();
-const camera = new PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.1, 10000 );
+// https://threejs.org/docs/#api/en/cameras/PerspectiveCamera
+const camera = new PerspectiveCamera( 10, aspect, 0.1, 20000 );
+camera.position.z = 8000;
+
+// https://threejs.org/docs/#api/en/cameras/OrthographicCamera
+const cameraOrtho = new OrthographicCamera(- 0.5 * frustumSize * aspect, 0.5 * frustumSize * aspect, frustumSize / 2, frustumSize / -2, 0.1, 10000);
+cameraOrtho.position.z = 5000;
 
 const renderer = new WebGLRenderer({ antialias: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
 document.body.appendChild( renderer.domElement );
 
 // Resize and update camera
-window.addEventListener('resize', function(e) {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+window.addEventListener('resize', onWindowResize);
 
 
-// Load SVG and extrude surface from SVG paths
+// Load SVG, extrude surface from SVG paths and Binary Intersect resulting Meshes
 // https://threejs.org/docs/#examples/en/loaders/SVGLoader
 const loader = new SVGLoader();
-const svgDataLeft = loader.parse(svg[2]);
-const svgDataRight = loader.parse(svg[3]);
+const material = new MeshNormalMaterial({ wireframe: false });
+
+
+/**
+ * @type {string[]}
+ */
+const svg = [
+    'M0 1100V0h660v1100H0Zm220-220V220h220v660H220Z',
+    'M220 220v660H0v220h660V880H440V0H0v220z',
+    'M220 660h440V0H0v220h440v220H0v660h660V880H220z',
+    'M0 1100h660V0H0v220h440v220H220v220h220v220H0z',
+    'M440 440H220V0H0v660h440v440h220V0H440z',
+    'M220 440h440v660H0V880h440V660H0V0h660v220H220z',
+    'M0 1100V0h660v220H220v220h440v660zm220-220V660h220v220z',
+    'M660 1100V0H0v220h440v880z',
+    'M660 0v1100H0V0zM220 880V660h220v220zm220-660v220H220V220z',
+    'M660 0v1100H0V880h440V660H0V0zM440 220v220H220V220z',
+].map(path => `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 660 1100" fill-rule="evenodd"><path d="${path}"/></svg>`);
+
+
+/**
+ * Parse all SVG text chunks.
+ *
+ * @type {SVGResult[]}
+ */
+const svgData = svg.map(loader.parse);
+
+/**
+ * Extrude Mesh for each set of SVG paths.
+ * Get only the first mesh from each list of meshes produced by extrusion.
+ *
+ * @type {Mesh[]}
+ */
+const meshes = svgData.map(svgResult => {
+    const meshList = MeshFromPath(svgResult.paths, true, material);
+    return meshList[0] ? meshList[0] : null;
+}).filter(Boolean);
+
+/**
+ * Produce Boolean Intersection for Meshes extruded from SVG.
+ *
+ * 1. Get extruded Mesh.
+ * 2. Get adjacent Mesh from the list and rotate it 90 deg along its vertical axis.
+ * 3. Boolean Intersect two meshes.
+ *
+ * @type {Mesh[]}
+ */
+const IntersectionMeshes = meshes.map((mesh, i, items) => {
+    const nextIndex = (i + 1) % items.length;
+
+    const meshRotated = items[nextIndex].clone();
+    meshRotated.rotateY( MathUtils.degToRad(90) ); // note scaleY(-1) applied later to the group
+    meshRotated.updateMatrix();
+
+    return CSG.intersect(mesh, meshRotated);
+});
 
 // Group we'll use for all SVG paths
 const group = new Group();
@@ -46,51 +104,49 @@ const group = new Group();
 // it happens in the process of mapping from 2d to 3d coordinate system
 group.scale.y *= -1;
 
-MeshFromPath(svgDataLeft.paths).forEach(mesh => group.add(mesh));
-MeshFromPath(svgDataRight.paths, true).forEach(mesh => group.add(mesh));
-
-// Boolean Intersection
-const intersection = CSG.intersect(group.children[0], group.children[1]);
-
-const intersectionGroup = new Group();
-intersectionGroup.scale.y *= -1;
-
-intersectionGroup.add(intersection);
-//intersectionGroup.remove(intersection);
-
-// Get group's size
-const box = new Box3().setFromObject(intersectionGroup);
-let vectorSize = new Vector3();
-box.getSize(vectorSize);
-
-// Offset each dimension half its length to center group elements
-intersectionGroup.children.forEach(item => {
-    item.translateOnAxis(vectorSize, -1/2);
-});
-
-// Axes helper
-// const axesHelper = new AxesHelper(1500);
-// intersectionGroup.add(axesHelper);
+// group.add(meshes[4]);
+let modelIndex = 0;
+group.add(IntersectionMeshes[modelIndex]);
 
 // Add intersection result to the scene
-scene.add(intersectionGroup);
+scene.add(group);
 
-camera.position.z = 5000;
 
-const rotationStep = MathUtils.degToRad(-0.6);
+let lastRotationPhase = 1;
+
+const rotationStep = MathUtils.degToRad(-1);
 
 function animate() {
     requestAnimationFrame( animate );
 
-    intersectionGroup.rotation.y += rotationStep;
+    group.rotateY(rotationStep);
 
-    renderer.render( scene, camera );
+    let rotationPhase = GetRotationQuadrant(group);
+
+    // Switch models every 90° of rotation
+    if (lastRotationPhase !== rotationPhase) {
+        group.remove(IntersectionMeshes[modelIndex]);
+
+        // Reset rotation
+        group.rotation.y = 0;
+        lastRotationPhase = 1;
+
+        // Switch to the next model in list
+        modelIndex = (modelIndex + 1) % IntersectionMeshes.length;
+        group.add(IntersectionMeshes[modelIndex]);
+
+        // Additional fanciness
+        // material.wireframe = !material.wireframe;
+    }
+
+    // renderer.render( scene, camera );
+    renderer.render( scene, cameraOrtho );
 }
 
 if ( WebGL.isWebGLAvailable() ) {
     animate();
-    console.log('DONE');
 } else {
+    console.error('WebGL is not available');
     const warning = WebGL.getWebGLErrorMessage();
     document.body.appendChild( warning );
 }
@@ -106,17 +162,20 @@ if ( WebGL.isWebGLAvailable() ) {
  *
  *
  * @param svgPath {ShapePath|ShapePath[]}
- * @param rotate {boolean}
+ * @param centerOrigin {boolean} Center origin inside bounding box.
+ *              Useful to ease rotations. Eliminating the need of translation or position move after rotation.
+ * @param material {Material}
  * @returns {Mesh[]}
  * @pure
  */
-function MeshFromPath(svgPath, rotate = false) {
-    const material = new MeshNormalMaterial({ wireframe: false });
-    const rightAngle = MathUtils.degToRad(90);
+function MeshFromPath(svgPath, centerOrigin = false, material = null) {
+    if (material === null) {
+        material = new MeshNormalMaterial({wireframe: true});
+    }
 
     if (Array.isArray(svgPath)) {
         return svgPath.reduce(
-            (acc, p) => acc.concat(MeshFromPath(p, rotate)),
+            (acc, p) => acc.concat(MeshFromPath(p, centerOrigin, material)),
             []
         );
     }
@@ -140,17 +199,17 @@ function MeshFromPath(svgPath, rotate = false) {
             bevelEnabled: false
         });
 
-        if (rotate) {
-            // Rotate geometry, not mesh
-            geometry.rotateY(rightAngle);
+        if (centerOrigin) {
+            // Get bounding box
+            geometry.computeBoundingBox();
+            let vectorSize = new Vector3();
+            geometry.boundingBox.getSize(vectorSize);
+
+            // Offset each dimension half its length to center origin inside bounding box
+            geometry.translate(vectorSize.x/-2, vectorSize.y/-2, vectorSize.z/-2);
         }
 
         const mesh = new Mesh(geometry, material);
-
-        if (rotate) {
-            // Shift along z-axis after rotation, to align inside original boundaries
-            mesh.position.z = shapeWidth;
-        }
 
         mesh.updateMatrix();
 
@@ -158,4 +217,65 @@ function MeshFromPath(svgPath, rotate = false) {
     });
 
     return result;
+}
+
+
+/**
+ * Get direction quadrant based on absolute rotation angle φ.
+ *
+ * 0: NE, φ ∈ [0; ½π)
+ * 1: NW, φ ∈ [½π; π)
+ * 2: SW, φ ∈ [π; ¾π)
+ * 3: SE, φ ∈ [¾π; 2π)
+ *
+ * Note: cardinal points NE/NW/SW/SE are ambiguous “directions”,
+ * used just for explainer here.
+ * North could not be derived just from the rotation angle.
+ *
+ * @param obj3d {Object3D}
+ * @returns {number}
+ * @constructor
+ */
+function GetRotationQuadrant(obj3d) {
+    const direction = new Vector3();
+    const PI = Math.PI;
+
+    // Short explainer on Quaternions and Euler angles
+    // https://discourse.threejs.org/t/when-i-rotate-an-object-how-do-i-know-its-true-angle-of-rotation/4573/9
+    // https://stackoverflow.com/a/34329880/1412330
+    obj3d.getWorldDirection(direction);
+    let a = Math.atan2(direction.x, direction.z);
+
+    // Convert interval: [-π; π] → [0; 2π)
+    a = ( a + PI ) % ( 2 * PI );
+
+    if (0 <= a && a < PI / 2) {
+        return 0;
+    } else if (PI / 2 <= a && a < PI) {
+        return 1;
+    } else if (PI <= a && a < PI + PI / 2) {
+        return 2;
+    } else if (PI + PI / 2 <= a && a < 2 * PI) {
+        return 3;
+    }
+
+    return 0;
+}
+
+
+function onWindowResize() {
+    SCREEN_WIDTH = window.innerWidth;
+    SCREEN_HEIGHT = window.innerHeight;
+    aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+    renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+
+    camera.aspect = aspect;
+    camera.updateProjectionMatrix();
+
+    cameraOrtho.left = - 0.5 * frustumSize * aspect;
+    cameraOrtho.right = 0.5 * frustumSize * aspect;
+    cameraOrtho.top = frustumSize / 2;
+    cameraOrtho.bottom = frustumSize / -2;
+    cameraOrtho.updateProjectionMatrix();
 }
