d7a1e505b55a88d9ad365a6d20fb6dbaf96855a8
Stepan Legachev <gitlog@sib.li>
2022-07-26 06:01:43 +0200

    refactor: cleanup

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Goals:
 
     Example: Extrude ring surface from `O` shape.
 
-- [ ] Boolean Intersect two perpendicular surfaces extracted that way.
+- [x] Boolean Intersect two perpendicular surfaces extracted that way.
   - https://github.com/sshirokov/ThreeBSP
   - https://github.com/manthrax/THREE-CSGMesh
   - https://github.com/Jiro-Digital/three-csg-ts
--- scene.js
+++ scene.js
@@ -48,8 +48,57 @@ const group = new Group();
 // it happens in the process of mapping from 2d to 3d coordinate system
 group.scale.y *= -1;
 
-const scaleFactor = 0.5;
-group.scale.multiplyScalar( scaleFactor );
+MeshFromPath(svgDataLeft.paths).forEach(mesh => group.add(mesh));
+MeshFromPath(svgDataRight.paths, true).forEach(mesh => group.add(mesh));
+
+// Boolean Intersection
+const intersection = CSG.intersect(...group.children);
+
+const intersectionGroup = new Group();
+intersectionGroup.scale.y *= -1;
+
+intersectionGroup.add(intersection);
+
+// Get group's size
+const box = new Box3().setFromObject(intersectionGroup);
+let vectorSize = new Vector3();
+box.getSize(vectorSize);
+
+// Offset each dimension half its length to center group elements
+// vectorSize.multiplyScalar(-0.5);
+intersectionGroup.children.forEach(item => {
+    item.translateOnAxis(vectorSize, -1/2);
+});
+
+// Axes helper
+if (DEBUG) {
+    const axesHelper = new AxesHelper(1500);
+    intersectionGroup.add(axesHelper);
+}
+
+// Add intersection result to the scene
+scene.add(intersectionGroup);
+
+camera.position.z = 5000;
+
+const rotationStep = MathUtils.degToRad(-0.6);
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    intersectionGroup.rotation.y += rotationStep;
+
+    renderer.render( scene, camera );
+}
+
+if ( WebGL.isWebGLAvailable() ) {
+    animate();
+    console.log('DONE');
+} else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.body.appendChild( warning );
+}
+
 
 
 /**
@@ -113,100 +162,4 @@ function MeshFromPath(svgPath, rotate = false) {
     });
 
     return result;
-}
-
-
-MeshFromPath(svgDataLeft.paths).forEach(mesh => group.add(mesh));
-MeshFromPath(svgDataRight.paths, true).forEach(mesh => group.add(mesh));
-
-
-svgDataRight.paths.forEach(path => {
-    const shapes = SVGLoader.createShapes( path );
-
-    // Each path has an array of shapes
-    shapes.forEach(shape => {
-        // Get width from shape []Vector2 coordinates
-        // Use extractPoints().shape to skip holes
-        const shapeWidth = shape.extractPoints().shape.reduce(
-            (acc, vec) => vec.width > acc ? vec.width : acc,
-            0
-        );
-
-        const intersection = CSG.intersect(group.children[0], group.children[1]);
-
-const intersectionGroup = new Group();
-intersectionGroup.scale.y *= -1;
-
-intersectionGroup.add(intersection);
-
-
-// Finally we can take each shape and extrude it
-        const geometry = new ExtrudeGeometry(shape, {
-            depth: shapeWidth,
-            bevelEnabled: false
-        });
-
-        // Rotate geometry, non mesh
-        geometry.rotateY(MathUtils.degToRad(90));
-
-        // Create a mesh and add it to the group
-        const mesh = new Mesh(geometry, material);
-
-        // X = red
-        // Y = green
-        // Z = blue
-
-        // Shift along z-axis after rotation
-        mesh.position.z = 660;
-        mesh.updateMatrix();
-        group.add(mesh);
-    });
-});
-
-
-const intersection = CSG.intersect(...group.children);
-
-const intersectionGroup = new Group();
-intersectionGroup.scale.y *= -1;
-
-intersectionGroup.add(intersection);
-
-// Get group's size
-const box = new Box3().setFromObject(intersectionGroup);
-let vectorSize = new Vector3();
-box.getSize(vectorSize);
-
-// Offset each dimension half its length to center group elements
-// vectorSize.multiplyScalar(-0.5);
-intersectionGroup.children.forEach(item => {
-    item.translateOnAxis(vectorSize, -1/2);
-});
-
-// Axes helper
-if (DEBUG) {
-    const axesHelper = new AxesHelper(1500);
-    intersectionGroup.add(axesHelper);
-}
-
-// Add intersection result to the scene
-scene.add(intersectionGroup);
-
-camera.position.z = 2000;
-
-const rotationStep = MathUtils.degToRad(-0.6);
-
-function animate() {
-    requestAnimationFrame( animate );
-
-    intersectionGroup.rotation.y += rotationStep;
-
-    renderer.render( scene, camera );
-}
-
-if ( WebGL.isWebGLAvailable() ) {
-    animate();
-    console.log('DONE');
-} else {
-    const warning = WebGL.getWebGLErrorMessage();
-    document.body.appendChild( warning );
 }
